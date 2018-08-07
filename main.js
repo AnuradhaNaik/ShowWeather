b99c1708c4a4e0d1ab5213991679d42020936c2c
Anuradha Naik <anuradha95.naik@gmail.com>
2018-08-07 14:30:53 -0400

    A lot has changed

---
+++ main.js
@@ -0,0 +1,27 @@
+var fivecities = { "NYC" : {
+      "id": 5128638,
+      "fullform": "New York",
+      "state": "NY"
+   },
+   "CHI": {
+      "id": 3582383,
+      "fullform": "Chicago",
+      "state": "IL"
+   },
+   "EWR": {
+      "id": 4143861,
+      "fullform": "Newark",
+      "state": "NJ"
+   },
+   "PHI": {
+      "id": 4560349,
+      "fullform": "Philadelphia",
+      "state": "PA"
+   },
+   "FAR": {
+      "id": "4758023",
+      "fullform": "FairFax",
+      "state": "VA"
+   }
+};
+
